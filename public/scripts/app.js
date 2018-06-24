666e2e78d23d8da6e15902950b366745a4a746ee
Prakhar Agrawal <prakhargvp@gmail.com>
2018-06-24 16:40:12 +0530

    Start with JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,62 @@
-"use strict";
+'use strict';
 
 console.log("App.js is Running...");
 
 // JSX - Javascript XML
 var template = React.createElement(
-  "p",
-  null,
-  " This is JSX from app.js! "
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		' Indecision App '
+	),
+	React.createElement(
+		'p',
+		null,
+		' This is some info! '
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'Item One'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item Two'
+		)
+	)
 );
 
+var userName = 'Prakhar';
+var userAge = 21;
+var userLocation = 'Dehradun';
+
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		userName
+	),
+	React.createElement(
+		'p',
+		null,
+		'Age: ',
+		userAge
+	),
+	React.createElement(
+		'p',
+		null,
+		'Location: ',
+		userLocation
+	)
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 console.log("App.js is Running...");
 
 // JSX - Javascript XML
-var template = <p> This is JSX from app.js! </p>
+var template = (
+	<div>
+		<h1> Indecision App </h1>
+		<p> This is some info! </p>
+		<ol>
+			<li>Item One</li>
+			<li>Item Two</li>
+		</ol>
+	</div>
+);
 
+var userName = 'Prakhar';
+var userAge  = 21;
+var userLocation = 'Dehradun';
+
+var templateTwo = (
+	<div>
+		<h1>{userName}</h1>
+		<p>Age: {userAge}</p>
+		<p>Location: {userLocation}</p>
+	</div>
+);
 var appRoot  = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
