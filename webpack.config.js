554e98ccbbcad67ac0524211106cab437f3889dd
Prakhar Agrawal <prakhargvp@gmail.com>
2018-07-01 16:54:13 +0530

    Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,10 @@ module.exports = {
 			exclude: /node_modules/
 		}]
 	},
-	devtool: 'cheap-module-eval-source-map'
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'public')
+	}
 };
 
 
